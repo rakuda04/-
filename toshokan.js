0eefc9986cfca9abeb51a38c35209535b25c2e79
rakuda04 <abdulkaderbaghaffar@gmail.com>
2024-01-28 16:38:43 +0300

    Added checkbox functionality & updated form inputs

--- toshokan.js
+++ toshokan.js
@@ -11,8 +11,20 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-   this.title = title
+    var title = document.getElementsByName("title")[0].value;
+    var author = document.getElementsByName("author")[0].value;
+    var pages = document.getElementsByName("page number")[0].value;
+    var readStatus = document.getElementsByName("read status")[0].value;
+    var newBook = new book(title, author, pages, readStatus);
+    myLibrary.push(newBook);
+    return newBook;
 }
+
+var submit = document.getElementById('sidePopUpForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var newBook = addBookToLibrary();
+    console.log(newBook);
+});
 
 function openPopup() {
     document.getElementsByClassName("sidePopup")[0].style.width = "40vw";
