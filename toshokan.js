f0be2b58e31c3210e2778f86c2be7fb159ff7305
rakuda04 <abdulkaderbaghaffar@gmail.com>
2024-01-29 19:07:45 +0300

    added dom creation and form validation

--- toshokan.js
+++ toshokan.js
@@ -14,10 +14,55 @@ function addBookToLibrary() {
     var title = document.getElementsByName("title")[0].value;
     var author = document.getElementsByName("author")[0].value;
     var pages = document.getElementsByName("page number")[0].value;
-    var readStatus = document.getElementsByName("read status")[0].value;
+
+    var titleRegex = /^[a-zA-Z0-9\s.,!?'"()]+$/;
+    var authorRegex = /^[a-zA-Z\s.,'-]+$/;
+    var pagesRegex = /^\d+$/;
+
+    if (!titleRegex.test(title)) {
+        alert("Title must start with a Capital letter and only contain letters, numbers, and punctuation");
+        return;
+    }
+
+    if (!authorRegex.test(author)) {
+        alert("author must start with a Capital letter and only contain letters");
+        return;
+    }
+
+    if (!pagesRegex.test(pages)) {
+        alert("should be a positive integer");
+        return;
+    }
+
     var newBook = new book(title, author, pages, readStatus);
     myLibrary.push(newBook);
-    return newBook;
+
+    
+    var bookDiv = document.createElement('div');
+    bookDiv.className = 'bookCard';
+
+    
+    bookDiv.innerHTML = `
+        <div class="bookCardHeader">
+            <div class="bookCardHeaderLeft"> 
+                <div class="bookCardTitle">${title}</div>
+                <div class="bookCardSubtitle">${author}</div>
+            </div>
+            <div class="bookCardHeaderRight">
+                <h4>${pages}</h4>
+            </div>
+        </div>
+        <div class="h-divider"></div>
+        <div class="bookCardBody">
+            <button type="button" onclick="myFunction()">読んだのか</button>
+            <button type="button" onclick="myFunction()">削除</button>
+        </div>
+        <div class="h-divider"></div>
+        <div class="bookCardFooter">${readStatus}</div>
+    `;
+
+    
+    document.body.appendChild(bookDiv);
 }
 
 var submit = document.getElementById('sidePopUpForm').addEventListener('submit', function(event) {
